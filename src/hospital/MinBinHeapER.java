76e3258b3337c05d5b98484e918255c931f65598
ravula7 <ravula7@ad.unc.edu>
2020-10-11 17:38:54 -0400

    commit

--- src/hospital/MinBinHeapER.java
+++ src/hospital/MinBinHeapER.java
@@ -31,38 +31,41 @@ public class MinBinHeapER  <V, P extends Comparable<P>> implements BinaryHeap<V,
     // TODO: enqueue
     @Override
     public void enqueue(V value, P priority) {
+        if (_heap == null) {
+            int currentIndex = 0;
+            _heap.add(currentIndex, new Patient<>(value, priority));
+        }
+        else {
+            int currentIndex = _heap.size() - 1; //wherever the new object was just added, this is the current index (last index)
+            //adding the new object (patient) (value and priority added to respective lists) to the end of the list
+            _heap.add(currentIndex, new Patient<>(value, priority));
 
-         int currentIndex = _heap.size()-1; //wherever the new object was just added, this is the current index (last index)
-        //adding the new object (patient) (value and priority added to respective lists) to the end of the list
-        _heap.add(currentIndex, new Patient<>(value,priority));
+            //bubble up (if needed)
+            while (currentIndex > 0) {
+                int parentIndex = (currentIndex - 1) / 2; //find parent of current
 
-        //bubble up (if needed)
-         while(currentIndex>0){
-            int parentIndex = (currentIndex-1)/2; //find parent of current
-             Patient<V, P> current = (Patient<V, P>) _heap.get(currentIndex); //current patient stored in case swap is necessary
-             Patient<V, P> parent = (Patient<V, P>) _heap.get(parentIndex); //parent patient stored in case swap is necessary
-            if(_heap.get(currentIndex).getPriority().compareTo(_heap.get(parentIndex).getPriority())<0){ //current priority is smaller than parent priority - need to swap!
-            //perform swap
-                //tmp stores the value at current index so that it doesn't get overridden when you set parent value at current index in the next step
-                Patient <V,P> tmp = (Patient<V, P>) current;
-                current = parent;
-                parent = tmp;
+                Patient<V, P> current = (Patient<V, P>) _heap.get(currentIndex); //current patient stored in case swap is necessary
+                Patient<V, P> parent = (Patient<V, P>) _heap.get(parentIndex); //parent patient stored in case swap is necessary
 
-            // Patient <V,P> tmp = (Patient<V, P>) _heap.get(currentIndex);
-            //_heap.set(currentIndex,(Patient<V, P>) _heap.get(parentIndex));
-           // _heap.set(parentIndex,tmp);
+                if (_heap.get(currentIndex).getPriority().compareTo(_heap.get(parentIndex).getPriority()) < 0) { //current priority is smaller than parent priority - need to swap!
+                    //perform swap
+                    //tmp stores the value at current index so that it doesn't get overridden when you set parent value at current index in the next step
+                    Patient<V, P> tmp = current;
+                    current = parent;
+                    parent = tmp;
 
-             currentIndex=parentIndex; //while loop exits once the parent index is 0 OR
-         }                              //when the current value isn't less than the root
-         else{
-             break;
-         }
+                    // Patient <V,P> tmp = (Patient<V, P>) _heap.get(currentIndex);
+                    //_heap.set(currentIndex,(Patient<V, P>) _heap.get(parentIndex));
+                    // _heap.set(parentIndex,tmp);
 
+                    currentIndex = parentIndex; //while loop exits once the parent index is 0 OR
+                }//when the current value isn't less than the root
+                else {
+                    break;
+                }
+            }
         }
     }
-
-
-
 
     // TODO: enqueue
     public void enqueue(V value) {
