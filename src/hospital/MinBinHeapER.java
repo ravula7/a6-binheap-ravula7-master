7789ca32168afaafba736ffbfd1819b0db390b42
ravula7 <ravula7@ad.unc.edu>
2020-10-12 21:51:36 -0400

    commit

--- src/hospital/MinBinHeapER.java
+++ src/hospital/MinBinHeapER.java
@@ -3,6 +3,8 @@ package hospital;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Collections;
+
 
 public class MinBinHeapER<V, P extends Comparable<P>> implements BinaryHeap<V, P> {
 
@@ -85,84 +87,58 @@ public class MinBinHeapER<V, P extends Comparable<P>> implements BinaryHeap<V, P
         if (_heap.size() == 0) {
             return null;
         }
-        if (_heap.size() == 1) {
-            V dequeuedValue = (V) _heap.get(0);//size is 1
-            _heap.remove(0); //size is 0 (has decremented by 1)
-            return dequeuedValue;
-        }
-        if (_heap.size() == 2) {
-            V dequeuedValue = (V) _heap.get(0); //size is 2
-            _heap.remove(0); //now size is 1
-            _heap.add(0, _heap.get(1)); //replace - size is 2
-            _heap.remove(1); //size back to 1 (has decremented by 1)
-            return dequeuedValue;
-        }
-        if (_heap.size() > 2) {
-            V dequeuedValue = (V) _heap.get(0); //original size = n
-            //fix the invariants
-            _heap.remove(0); //size-1=n-1
-            _heap.add(0, _heap.get(size())); //replace - size is n
-            _heap.remove(size()); //size back to n-1 (has decremented by 1)
+        //when heap isn't empty
+        //get value at 0, set value at 0 to last item, remove last item so no repeats
+        V dequeuedValue = _heap.get(0).getValue();
+        _heap.set(0,_heap.get(size()-1));
+        _heap.remove(_heap.size()-1); //size is now decremented by 1
 
-            int currentIndex = 0;
-            int leftIndex = (currentIndex * 2) + 1;
-            int rightIndex = (currentIndex * 2) + 2;
+        //calculate indexes to check what can replace the value at currentIndex if needed through swap
+        int currentIndex = 0;
+        int leftIndex = (currentIndex*2)+1;
+        int rightIndex = (currentIndex*2)+2;
 
-            //while not a leaf
-            while (_heap.get(leftIndex) != null || _heap.get(rightIndex) != null) {
-                //left and right child
-                if (_heap.get(leftIndex) != null && _heap.get(rightIndex) != null) {
-                    //when left is smaller than current
-                    if (_heap.get(leftIndex).getPriority().compareTo(_heap.get(rightIndex).getPriority()) < 0) {
-                        if (_heap.get(leftIndex).getPriority().compareTo(_heap.get(currentIndex).getPriority()) < 0) {
-                            //swap left and current value
-                            Patient<V, P> tmp = (Patient) _heap.get(leftIndex);
-                            _heap.set(leftIndex, _heap.get(currentIndex));
-                            _heap.set(currentIndex, tmp);
-                            //update indexes
-                            currentIndex = leftIndex;
-                            leftIndex = (currentIndex * 2) + 1;
-                            rightIndex = (currentIndex * 2) + 2;
-                        } else {
-                            break;
-                        } //while loop will end once children are not less than current
-                    }
-                    //when right is smaller than current
-                    if (_heap.get(rightIndex).getPriority().compareTo(_heap.get(leftIndex).getPriority()) < 0) {
-                        if (_heap.get(rightIndex).getPriority().compareTo(_heap.get(currentIndex).getPriority()) < 0) {
-                            //swap right and current value
-                            Patient<V, P> tmp = (Patient) _heap.get(rightIndex);
-                            _heap.set(rightIndex, _heap.get(currentIndex));
-                            _heap.set(currentIndex, tmp);
-                            //update indexes
-                            currentIndex = rightIndex;
-                            leftIndex = (currentIndex * 2) + 1;
-                            rightIndex = (currentIndex * 2) + 2;
-                        } else {
-                            break;
-                        } //while loop will end once children are not less than current
-                    }
+        //while leaf --> left index isn't valid (which means there is no left leaf, so why would there be a right leaf)
+        while(!(leftIndex >= size()-1)){
+            //only left child
+            if((leftIndex == size()-1)){ //if left index is at the end
+                //if left child is less than current child, swap
+                if(_heap.get(leftIndex).getPriority().compareTo(_heap.get(currentIndex).getPriority())<0){
+                    Collections.swap(_heap, currentIndex, leftIndex);
+                    currentIndex = leftIndex;
                 }
-                //left child
-                if (_heap.get(leftIndex) != null && _heap.get(rightIndex) == null) {
-                    //when left is smaller than current
-                    if (_heap.get(leftIndex).getPriority().compareTo(_heap.get(currentIndex).getPriority()) < 0) {
-                        //swap left and current value
-                        Patient<V, P> tmp = (Patient) _heap.get(leftIndex);
-                        _heap.set(leftIndex, _heap.get(currentIndex));
-                        _heap.set(currentIndex, tmp);
-                        //update indexes
+                //if that's not the case, then break -- invariants have been fixed
+                else{
+                    break;
+                }
+            }
+            //both left and right children
+            else {
+                //if left value is less than right value and current value, then swap. else break
+                if(_heap.get(leftIndex).getPriority().compareTo(_heap.get(rightIndex).getPriority())<0){
+                    if(_heap.get(leftIndex).getPriority().compareTo(_heap.get(currentIndex).getPriority())<0){
+                        Collections.swap(_heap,currentIndex,leftIndex);
                         currentIndex = leftIndex;
-                        leftIndex = (currentIndex * 2) + 1;
-                        rightIndex = (currentIndex * 2) + 2;
-                    } else {
-                        break;
-                    } //while loop will end once children are not less than current or when there is no left or right child
+                    }
+                    else{
+                       break;
+                    }
                 }
-            } //while loop will end once leaf
-            return dequeuedValue;
+                //otherwise, right value is less than left value
+                else {
+                    //if right value is less than current value, then swap. else break
+                    if(_heap.get(rightIndex).getPriority().compareTo(_heap.get(currentIndex).getPriority()) <0){
+                        Collections.swap(_heap, currentIndex,rightIndex);
+                        currentIndex = rightIndex;
+                    }
+                    else {
+                       break;
+                    }
+                }
+            }
         }
-        return null;
+        //return the dequeued value now that all of the invariants are fixed
+        return dequeuedValue;
     }
 
 
