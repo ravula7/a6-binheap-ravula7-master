d82f4e2d0203f85e0573fbc09c405677aa4c161e
ravula7 <ravula7@ad.unc.edu>
2020-10-12 20:30:03 -0400

    commit

--- src/hospital/MinBinHeapER.java
+++ src/hospital/MinBinHeapER.java
@@ -123,9 +123,6 @@ public class MinBinHeapER<V, P extends Comparable<P>> implements BinaryHeap<V, P
                             currentIndex = leftIndex;
                             leftIndex = (currentIndex * 2) + 1;
                             rightIndex = (currentIndex * 2) + 2;
-                            if(leftIndex == -1 && rightIndex == -1){
-                                break;
-                            }
                         } else {
                             break;
                         } //while loop will end once children are not less than current
@@ -141,9 +138,6 @@ public class MinBinHeapER<V, P extends Comparable<P>> implements BinaryHeap<V, P
                             currentIndex = rightIndex;
                             leftIndex = (currentIndex * 2) + 1;
                             rightIndex = (currentIndex * 2) + 2;
-                            if(leftIndex == -1 && rightIndex == -1){
-                                break;
-                            }
                         } else {
                             break;
                         } //while loop will end once children are not less than current
@@ -161,9 +155,6 @@ public class MinBinHeapER<V, P extends Comparable<P>> implements BinaryHeap<V, P
                         currentIndex = leftIndex;
                         leftIndex = (currentIndex * 2) + 1;
                         rightIndex = (currentIndex * 2) + 2;
-                        if(leftIndex == -1 && rightIndex == -1){
-                            break;
-                        }
                     } else {
                         break;
                     } //while loop will end once children are not less than current or when there is no left or right child
